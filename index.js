f0c756cc6d0b1cd4d552666c9d31c159d5d24355
Taylor <bKuzzcs@gmail.com>
2022-02-19 12:16:22 -0600

    commit tree 336e0407016988bd258fbf46c5f8e7b795fc00bb

---
+++ index.js
@@ -0,0 +1,38 @@
+let number = 10
+
+function add() {
+    return (number + 5)
+}
+function subtract() {
+    return (number - 5)
+}
+function multiply() {
+    return (number * 10)
+}
+function divide() {
+    return (number / 4)
+}
+function add(a, b) {
+    return (a + b)
+}
+function subtract(a, b) {
+    return (a - b)
+}
+function multiply(a, b) {
+    return (a * b)
+}
+function divide(a, b) {
+    return (a / b)
+}
+function increment(a) {
+    return (a+ 1)
+}
+function decrement(a) {
+    return (a -1)
+}
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
